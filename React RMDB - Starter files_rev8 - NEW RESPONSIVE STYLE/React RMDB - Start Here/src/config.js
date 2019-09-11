75cbed0a32aa428a8edc73fb3ff82d04a6f69280
Antonio Tobar <antonio.tobar.dev@gmail.com>
2019-09-11 18:19:16 -0500

    api key added

--- React RMDB - Starter files_rev8 - NEW RESPONSIVE STYLE/React RMDB - Start Here/src/config.js
+++ React RMDB - Starter files_rev8 - NEW RESPONSIVE STYLE/React RMDB - Start Here/src/config.js
@@ -2,7 +2,7 @@
 // To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+const API_KEY = 'b2dc1affb27b024e4bd237fb2d1e9b4d';
 
 // Images
 // An image URL looks like this example:
