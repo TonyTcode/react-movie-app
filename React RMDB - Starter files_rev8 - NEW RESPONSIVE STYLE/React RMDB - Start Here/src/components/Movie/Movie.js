54b2a1abf57e38d7f87b11d5d8897b98f60de530
Antonio Tobar <antonio.tobar.dev@gmail.com>
2019-09-16 10:26:42 -0500

    time info updated

--- React RMDB - Starter files_rev8 - NEW RESPONSIVE STYLE/React RMDB - Start Here/src/components/Movie/Movie.js
+++ React RMDB - Starter files_rev8 - NEW RESPONSIVE STYLE/React RMDB - Start Here/src/components/Movie/Movie.js
@@ -56,7 +56,7 @@ class Movie extends Component {
                     <div>
                         <Navigation movie={this.props.location.movieName} />
                         <MovieInfo movie={this.state.movie} directors={this.state.directors} />
-                        <MovieInfoBar movie={this.state.movie.runtime} budget={this.state.movie.budget} revenue={this.state.movie.revenue} />
+                        <MovieInfoBar time={this.state.movie.runtime} budget={this.state.movie.budget} revenue={this.state.movie.revenue} />
                     </div>
                 : null }
                 {this.state.actors ? 
--- React RMDB - Starter files_rev8 - NEW RESPONSIVE STYLE/React RMDB - Start Here/src/components/elements/MovieInfoBar/MovieInfoBar.js
+++ React RMDB - Starter files_rev8 - NEW RESPONSIVE STYLE/React RMDB - Start Here/src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
-import { calcTime, convertMoney } from '../../../helpers'
+import { calcTime, convertMoney } from '../../../helpers.js';
 import './MovieInfoBar.css';
 
 const MovieInfoBar = (props) => {
