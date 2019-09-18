6e32f8cd5cadbcf3b0c0488e6eb8bc24ec227207
Antonio Tobar <antonio.tobar.dev@gmail.com>
2019-09-18 16:08:08 -0500

    loadMoreBtn fixed

--- React RMDB - Starter files_rev8 - NEW RESPONSIVE STYLE/React RMDB - Start Here/src/components/elements/LoadMoreBtn/LoadMoreBtn.js
+++ React RMDB - Starter files_rev8 - NEW RESPONSIVE STYLE/React RMDB - Start Here/src/components/elements/LoadMoreBtn/LoadMoreBtn.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './LoadMoreBtn.css'
 
 const LoadMoreBtn = ({ onClick, text }) => (
-        <div className="rmdb-loadmorebtn" onClick={onClick}>
+        <div className="rmdb-loadmorebtn" onClick={() => onClick(true)}>
            <p>{text}</p>
         </div>
     )
