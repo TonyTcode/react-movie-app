c9eac94d2dbeda7867ea86469c6f1bd644968029
Antonio Tobar <antonio.tobar.dev@gmail.com>
2019-09-30 19:52:38 -0500

    deployment

---
+++ React RMDB - Starter files_rev8 - NEW RESPONSIVE STYLE/React RMDB - Start Here/build/precache-manifest.cf8a5c87eb9f61ec7e701f410845a71e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d6adf3dbf047329fda785e59ddbaec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d8425214bc1ddff43ba",
+    "url": "/static/css/main.d4164829.chunk.css"
+  },
+  {
+    "revision": "4e78faefdfa2401be1f3",
+    "url": "/static/js/2.b475aae1.chunk.js"
+  },
+  {
+    "revision": "2d8425214bc1ddff43ba",
+    "url": "/static/js/main.95a72b73.chunk.js"
+  },
+  {
+    "revision": "5e2bbe5c72ac3dcf6bd1",
+    "url": "/static/js/runtime-main.b106d53e.js"
+  }
+]);
